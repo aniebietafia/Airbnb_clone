cd5b196f39e0a6ce37f9247ee8c37737291f873a
Aniebiet Afia <38618247+aniebietafia@users.noreply.github.com>
2022-11-25 19:07:45 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
-Defines review class
+Module that defines a class Review inherit from BaseModel
 """
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Reviews made by users about a place"""
+    """Class Review"""
     place_id = ""
     user_id = ""
     text = ""
+
+    def __init__(self, *args, **kwargs):
+        """init model"""
+        super().__init__(*args, **kwargs)
