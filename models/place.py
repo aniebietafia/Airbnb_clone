8142cb366c22edfb25054ce8ab7f9797013b8810
Aniebiet Afia <38618247+aniebietafia@users.noreply.github.com>
2022-11-25 19:06:44 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 """
-Defines Place class
+Module that defines a class Place inherit from BaseModel
 """
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """Defines Place class"""
+    """Class Place"""
     city_id = ""
     user_id = ""
     name = ""
     description = ""
-    number_bathrooms = 0
     number_rooms = 0
+    number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """init model"""
+        super().__init__(*args, **kwargs)
